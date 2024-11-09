9a479512a356d13b48cf7aa27e94036747b1d1e6
ul4sfak1r <ulasfakir66@gmail.com>
2024-11-09 11:13:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const subscribeButton = document.getElementById('subscribe-button');
+const successDiv = document.getElementById('success-div');
+const emailInput = document.querySelector('input[type="email"]');
+const mainDiv = document.getElementById('main-div');
+const successMessage = document.getElementById('success-message');
+const labelDiv = document.getElementById('label-message');
+
+subscribeButton.addEventListener("click", (event) => {
+
+	event.preventDefault();
+
+	function validateEmailFormat(email) {
+    	const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    	return emailPattern.test(email);
+	}
+
+	if (validateEmailFormat(emailInput.value)) {
+		mainDiv.classList.add('hidden');
+
+		successDiv.classList.remove('hidden');
+		successMessage.innerHTML = `A confirmation email has been sent to <b>${emailInput.value}</b>. Please open it and click the button inside to confirm your subscription.`;
+	} else {
+		emailInput.classList.add('error');
+		labelDiv.insertAdjacentHTML('beforeend', `<span style="color: hsl(4, 100%, 67%)">Valid email required</span>`);
+	}
+})
